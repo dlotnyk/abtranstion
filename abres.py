643eaace62eaaebea0853a67b58f1294b8309371
dlotnykCorn <dmytro.lotnyk@gmail.com>
2019-01-14 16:28:24 -0500

    import and save date/time

--- abres.py
+++ abres.py
@@ -65,8 +65,29 @@ class abdata():
         self.cursor.close()
         self.cnx.close()
         print('Disconnected!')
-
-
+#-------------------------------------------------------------
+    @my_logger
+    @time_this
+    def import_fun(self):
+        '''import needed data from dat files'''
+        path="c:\\Users\\JMP\\Documents\\Thermal Conductivity\\Backup\\2019JAN\\20190102\\HEC2p2mK.dat"
+#        data=np.genfromtxt(path, unpack=True, skip_header=1, usecols = (4, 5, 2, 6, 13, 7))
+        data=np.genfromtxt(path, unpack=True, skip_header=1)
+        print(data[0][0],data[1][0],data[2][0])
+        with open(path,'r') as f:
+            line=f.readline()
+            line=f.readline()
+            d=line.split()
+            mm,dd,yy=d[0].split('/')
+            print("year {}, month {}, day {}".format(yy,mm,dd))
+            hh,mins,ss=d[1].split(':')
+            print("hour {}: min {}: sec {}".format(hh,mins,ss))
+            t1,t2=str(datetime.datetime.now()).split()
+            print(t2)
+            a=datetime.datetime(int('20'+yy),int(mm),int(dd),int(hh),int(mins),int(ss),0)
+            print(a)
+            
 conf_loc='ab_data.db'
 A=abdata(conf_loc)
+A.import_fun()
 A.close_f()
