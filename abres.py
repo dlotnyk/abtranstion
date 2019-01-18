645c6575d772d62cafa620305190a3c017b46361
dlotnykCorn <dmytro.lotnyk@gmail.com>
2019-01-18 16:43:03 -0500

    Database created from Nov 28 to Jan 11. 48 mb

--- abres.py
+++ abres.py
@@ -29,14 +29,12 @@ def my_logger(orig_func):
     logging.basicConfig(filename='work.log'.format(orig_func.__name__), level=logging.INFO)
     @wraps(orig_func)
     def wrapper(*args,**kwargs):
-        frm = str(inspect.stack()[1][0])
-        ff1=str(frm.split(' code ')[1])
-        ff2=str(ff1.split('>')[0])
+        frm = str(inspect.stack()[1][4][0])
         dt=datetime.datetime.now()
         dt_str=str(dt)
         vrema=dt_str.split('.')[0]
         logging.info(
-                ' {} Ran with args: {}, and kwargs: {} \n'.format(vrema, args,kwargs))
+                ' {} Ran with args:{}, {}, and kwargs: {}'.format(vrema,frm[0:-1],args,kwargs))
         return orig_func(*args, **kwargs) 
     return wrapper 
 #-------------------------------------------------------------
@@ -44,10 +42,13 @@ def my_logger(orig_func):
 class abdata():
     '''read from fies, create a sql database from date, universal time, Q, Tmc and pressure'''
     
-    test_path="c:\\dima\\proj\\ab_trans\\20181201\\"
-    
-    def __init__(self,conf):
+#    test_path="c:\\dima\\proj\\ab_trans\\20181201\\"
+    hec_file="HEC_combine.dat"
+    ic_file="IC_combine.dat"
+    work_dir=""
+    def __init__(self,conf,data_dir):
         self.conf=conf
+        self.data_dir=data_dir
         self.connect_loc(conf)
 #-------------------------------------------------------------
     def __repr__(self):
@@ -73,20 +74,13 @@ class abdata():
 #-------------------------------------------------------------
     @my_logger
     @time_this
-    def import_fun(self):
+    def import_fun(self,path_press):
         '''import needed data from dat files
         time/date [0] [1]; unitime [2]; Q [6]; Tmc [13]
         update an sql table'''
-        path="c:\\Users\\JMP\\Documents\\Thermal Conductivity\\Backup\\2019JAN\\20190102\\HEC2p2mK.dat"
-        path1="c:\\Users\\JMP\\Documents\\Thermal Conductivity\\Backup\\2019JAN\\20190102\\IC2p2mK.dat"
-        path2="c:\\Users\\JMP\\Documents\\Thermal Conductivity\\Backup\\2019JAN\\20190102\\pressure_log20190102.dat"
-        ps=path.split('\\')
-        ls=len(ps[-1])
-#        print(ps[-1])
-#        print(path[0:-ls])
-#        data=np.genfromtxt(path, unpack=True, skip_header=1, usecols = (4, 5, 2, 6, 13, 7))
-#        data=np.genfromtxt(path, unpack=True, skip_header=1)
-#        print(data[0][0],data[1][0],data[2][0])
+        path = self.work_dir+self.hec_file
+        path1=self.work_dir+self.ic_file
+        path2=self.work_dir+path_press[0]
         with open(path,'r') as f, open(path1,'r') as f1, open(path2,'r') as f2:
             next(f) # skip first
             next(f1)
@@ -114,8 +108,7 @@ class abdata():
     @time_this
     def find_files(self):
         '''find dat files in directory'''
-#        dir1="c:\\Users\\JMP\\Documents\\Thermal Conductivity\\Backup\\2019JAN\\20190102\\"
-        for root,dirs,files in os.walk(self.test_path):
+        for root,dirs,files in os.walk(self.work_dir):
             ll={os.path.join(root,file) for file in files if file.endswith(".dat")}
         return ll
 #-------------------------------------------------------------    
@@ -133,27 +126,29 @@ class abdata():
 #               "id integer PRIMARY KEY, "
                "date text NOT NULL, "
                "uni_time integer NOT NULL, "
-               "Q1 real NOT NULL, "
-               "Q2 real NOT NULL, "
+               "Q1 real, "
+               "Q2 real, "
                "Tmc real, "
-               "pressure real NOT NULL);".format(db_name))
+               "pressure real);".format(db_name))
         self.cursor.execute(query)
 #-------------------------------------------------------------    
     @my_logger
     @time_this
-    def drop_table(self,db_name):  
+    def drop_table(self,tb_name):  
         '''drop db_name table'''
-        query=("DROP TABLE {}".format(db_name))
+        query=("DROP TABLE {}".format(tb_name))
         self.cursor.execute(query)
 #-------------------------------------------------------------    
-    def __update_table(self,db_name,values):
+    def __update_table(self,tb_name,values):
         '''set a new values to the table'''
-        query="INSERT INTO %s (date, uni_time, Q1, Q2, Tmc, pressure) VALUES (?, ?, ?, ?, ?, ?) " % db_name
+        query="INSERT INTO %s (date, uni_time, Q1, Q2, Tmc, pressure) VALUES (?, ?, ?, ?, ?, ?) " % tb_name
         self.cursor.execute(query,values)
 #-------------------------------------------------------------            
+    @my_logger
+    @time_this
     def select_vals(self,tb_name):
         '''select all values'''
-        query="SELECT * FROM %s WHERE 1" % tb_name
+        query="SELECT * FROM %s WHERE 1  ORDER BY date ASC" % tb_name
         self.cursor.execute(query)
         data=self.cursor.fetchall()
         res=self._removeNull(data)
@@ -173,40 +168,53 @@ class abdata():
             dat[ind]=x
         return dat       
     #-------------------------------------------------------------            
+    @my_logger
+    @time_this
     def find_hec(self,files):
         '''find all HEC and IC files. combine them into single HEC and IC. find a path with pressure'''
-        hec_f=self.test_path+"HEC_combine.dat"
-        ic_f=self.test_path+"IC_combine.dat"
+        hec_f=self.work_dir+self.hec_file
+        ic_f=self.work_dir+self.ic_file
         path_press=sorted([k.split('\\')[-1] for k in files if k.split('\\')[-1][0:3]=='pre'])
         hec=sorted([jj.split('\\')[-1] for jj in files if jj.split('\\')[-1][0:3]=='HEC'
-                    and jj.split('\\')[-1] != "HEC_combine.dat"])
+                    and jj.split('\\')[-1] != self.hec_file])
         ic=sorted([ii.split('\\')[-1] for ii in files if ii.split('\\')[-1][0:2]=='IC'
-                   and ii.split('\\')[-1] != "IC_combine.dat"])
-#        print(hec,ic)
+                   and ii.split('\\')[-1] != self.ic_file])
         with open (hec_f, 'w') as outfile:
             for f in hec:
-                with open(self.test_path+f,'r') as infile:
+                with open(self.work_dir+f,'r') as infile:
                     next(infile)
                     for line in infile:
                         outfile.write(line)
                         
         with open (ic_f, 'w') as outfile1:
             for f1 in ic:
-                with open(self.test_path+f1,'r') as infile1:
+                with open(self.work_dir+f1,'r') as infile1:
                     next(infile1)
                     for line1 in infile1:
                         outfile1.write(line1)
         return path_press
+#-------------------------------------------------------------   
+    @my_logger
+    @time_this
+    def dir_scan(self):
+        '''scan through he data directory. find all and sort
+        self.work_dir is changed here.'''
+        l_com = [dirs for root,dirs,files in os.walk(self.data_dir)]
+        l_dir = sorted(l_com[0])
+        for ii in l_dir:
+            print(ii)
+            self.work_dir=self.data_dir+ii+"\\"
+            files=self.find_files()
+            path_p=self.find_hec(files)
+            self.import_fun(path_p)
 
 #-------------main--------------------------------------------   
 if __name__ == '__main__':       
     conf_loc='ab_data.db'
-    A=abdata(conf_loc)
-    files=A.find_files()
-    path_p=A.find_hec(files)
-#    print(files)
+    data_dir="c:\\dima\\proj\\ab_trans\\data\\"
+    A=abdata(conf_loc,data_dir)
 #    A.drop_table('my_t')
-#    A.create_table('my_t')
-#    A.import_fun()
-#    data1=A.select_vals('my_t')
+    A.create_table('my_t')
+    A.dir_scan()
+    data1=A.select_vals('my_t')
     A.close_f()
